de695fc9f38b8bc730b183517bd7f394614e45bc
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2021-11-12 13:48:14 +0000

    solving connection query

---
+++ index.js
@@ -0,0 +1,27 @@
+let mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: 'proj-mysql.uopnet.plymouth.ac.uk',
+    user: 'COMP3000_TGardner',
+    password: 'TMOZwaZUaawEZeN2',
+    database: 'COMP3000_TGardner'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS Solution', function (error, results, fields){
+    if (error) throw error;
+    console.log('The solution is: ', result[0].solution);
+});
+
+connection.end();
+
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+//
+//     console.log('connected as id ' + connection.threadId);
+// });
+
+
