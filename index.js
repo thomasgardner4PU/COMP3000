a6189d35253783c74371974fcbbd27005461e9b3
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2021-11-13 17:30:43 +0000

    rebuilt project - re-added files

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ let connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS Solution', function (error, results, fields){
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields){
     if (error) throw error;
-    console.log('The solution is: ', result[0].solution);
+    console.log('The solution is: ', results[0].solution);
 });
 
 connection.end();
