d1d4ca6844c8540926e78a2bd18e9082d6f8744a
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2021-12-31 16:22:52 +0000

    updated gitignore file & edited app.js

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
 const express = require("express")
+const mysql = require("mysql")
+const dotenv = require("dotenv")
+
+// dotenv.config({ path: './.env' });
+
+// dotenv.config({ path:__dirname + '/.env' })
+
+const path = require('path')
+require('dotenv').config({
+    path: path.resolve(__dirname, '/.env')
+})
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: process.env["DATABASE_HOST"],
+    user: process.env["DATABASE_USER"],
+    password: process.env["DATABASE_PASSWORD"],
+    database: process.env["DATABASE"]
+})
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
 })
 
 app.listen(5000, () => {
-    console.log("Server started on Port 5000")
+    console.log("Server started on Port 5001")
 })
 
+//
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields){
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+// });
+//
+// connection.end();
 
-let mysql = require('mysql');
-let connection = mysql.createConnection({
-    host: 'proj-mysql.uopnet.plymouth.ac.uk',
-    user: 'COMP3000_TGardner',
-    password: 'TMOZwaZUaawEZeN2',
-    database: 'COMP3000_TGardner'
-});
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields){
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-});
-
-connection.end();
-
-// connection.connect(function (err) {
+// db.connect(function (err) {
 //     if (err) {
 //         console.error('error connecting: ' + err.stack);
 //         return;
 //     }
 //
-//     console.log('connected as id ' + connection.threadId);
+//     console.log('connected as id ' + db.threadId);
 // });
+
+db.connect( (error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("Mysql Connected...")
+    }
+})
