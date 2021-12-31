754a9ae868871bed7d2792d7fdd9269f65abe86c
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2021-12-31 16:36:25 +0000

    fixed dotenv issue with connection to node

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const dotenv = require("dotenv")
 
 // dotenv.config({ path:__dirname + '/.env' })
 
-const path = require('path')
-require('dotenv').config({
-    path: path.resolve(__dirname, '/.env')
-})
+require('dotenv').config();
+
+// const path = require('path')
+// require('dotenv').config({
+//     path: path.resolve(__dirname, '/.env')
+// })
 
 const app = express();
 
