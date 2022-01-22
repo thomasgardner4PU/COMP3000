0158ce7675aef161148c1599e58cf6a388cd1e4a
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2022-01-22 13:36:16 +0000

    still sorting out node error when accessing profile

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const mysql = require("mysql");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const {hashPassword} = require("mysql/lib/protocol/Auth");
+const { promisify } = require('util')
+const {decode} = require("jsonwebtoken");
 
 const db = mysql.createConnection({
     host: process.env["DATABASE_HOST"],
@@ -91,4 +93,37 @@ exports.register = (req, res) => {
             }
         })
     });
+}
+exports.isLoggedIn = async (req, res, next) => {
+    console.log(req.cookies)
+    if ( req.cookies.jwtid) {
+        try {
+            //1) verify the token
+            const decoded = await promisify(jwt.verify)(req.cookies.jwtid,
+                process.env.JWT_SECRET
+            );
+
+            console.log(decoded)
+            //
+            // // 2) check if the user still exists
+            // db.query('SELECT * WHERE id = ?', [decoded.id], (error, result) => {
+            //     console.log(result);
+            //
+            //     if (!result) {
+            //         return next();
+            //     }
+            //
+            //     req.user = result[0]
+            //     return next();
+            // });
+        } catch (error) {
+            console.log(error)
+            return next();
+        }
+
+
+    } else {
+        next();
+    }
+
 }
--- routes/pages.js
+++ routes/pages.js
@@ -1,5 +1,6 @@
 let express = require('express');
 const {route} = require("express/lib/router");
+const authController = require('../controllers/auth')
 
 let router = express.Router();
 
@@ -15,7 +16,12 @@ router.get('/login', (req, res) =>{
     res.render('login');
 });
 
-router.get('/profile', (req, res) => {
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+    // if ( req.user ) {
+    //     res.render('profile')
+    // } else {
+    //     res.redirect('/login')
+    // }
     res.render('profile');
 })
 
