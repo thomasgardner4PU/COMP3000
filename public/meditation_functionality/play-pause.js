818212f79c6ff2084cc106e661909394498760ba
tom <72255127+thomasgardner4PU@users.noreply.github.com>
2022-03-22 08:41:58 +0000

    issue with play audio

---
+++ public/meditation_functionality/play-pause.js
@@ -0,0 +1,14 @@
+// select the play and pause buttons
+
+const play = document.querySelector('.play'),
+    pause = document.querySelector('.pause');
+    // select audio element
+const audio = document.querySelector('.audio audio');
+
+play.addEventListener('click', () => {
+        audio.play();
+    })
+
+play.addEventListener('click', () => {
+    audio.pause();
+})
