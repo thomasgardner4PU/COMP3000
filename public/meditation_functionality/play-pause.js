41580fe8aae6867cf94ae005b1034c72dc4b1f80
tom <72255127+thomasgardner4PU@users.noreply.github.com>
2022-03-28 20:23:32 +0100

    started duration of audio functionality for audio file
    audio will have multiple durations and also default duration for when user opens this view

--- public/meditation_functionality/play-pause.js
+++ public/meditation_functionality/play-pause.js
@@ -3,25 +3,42 @@ const play = document.querySelector('.play'),
     pause = document.querySelector('.pause');
     // select audio element
 const audio = document.querySelector('.audio audio');
-
 play.addEventListener('click', () => {
         audio.play();
-    });
+});
 
+// play the audio file
 play.addEventListener('click', () => {
     audio.play();
 });
 
+// pause the audio file
 pause.addEventListener('click', () => {
     audio.pause();
 });
 
-//select seasons and video
+//select video's for seasons
 const seasons = document.querySelectorAll(".season");
 video = document.querySelector(".video video");
 
+// change background video
 seasons.forEach( season => {
     season.addEventListener('click', () => {
         video.src = season.getAttribute("video-src");
+    });
+});
+
+//select durations buttons
+
+const duration = document.querySelectorAll('.duration');
+
+// default audio durations
+let audioDuration = 120; // 2minutes
+
+// chnage audio duration
+
+duration.forEach( duration => {
+    duration.addEventListener('click', () => {
+        audioDuration = duration.getAttribute('audio-duration');
     })
 })
