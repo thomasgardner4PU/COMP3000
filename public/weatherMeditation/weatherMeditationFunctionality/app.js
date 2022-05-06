e67b5e1062c00213dfc086d1ba2c0e1d7dda5986
tom <72255127+thomasgardner4PU@users.noreply.github.com>
2022-05-07 00:02:38 +0100

    changed class name

---
+++ public/weatherMeditation/weatherMeditationFunctionality/app.js
@@ -0,0 +1,60 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+
+    //sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    // get the length of the outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+
+    //duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // play sound
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    
+
+    // create a function to stop and play sounds
+    const checkPlaying = song => {
+        if (song.paused){
+            song.play();
+            video.play();
+            play.src = '/weatherMeditation/svg/pause.svg'
+        } else {
+            song.pause();
+            video.pause();
+            play.src = '/weatherMeditation/svg/play.svg'
+        }
+    }
+
+    // animate the circle
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        //animate the circle
+
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        // animate the text
+
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+    }
+}
+
+app();
